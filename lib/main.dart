d1a091ccff5821e01ebee915a1067f63861fd0b1
Jecko <jeckojthottungal@yahoo.com>
2023-04-09 17:51:10 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:pushtestapp/homescreen.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //Initialize firebase messaging service
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
   //options: DefaultFirebaseOptions.currentPlatform,
   //    );
 
   runApp(const MyApp());
+
+  OneSignal.shared.setLogLevel(OSLogLevel.debug, OSLogLevel.none);
+  OneSignal.shared.setAppId('19a211d2-e82c-4ed7-975d-a3fade2c4e03');
+  OneSignal.shared
+      .promptUserForPushNotificationPermission()
+      .then((accepted) {});
 }
 
 class MyApp extends StatelessWidget {
